4774b4c50a3d771e34687b24e95c6ce59b869507
GitHub Action <action@github.com>
2020-07-29 11:27:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/lurunze/mybatissql/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lurunze/mybatissql/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lurunze.mybatissql.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lurunze.mybatissql.MyBundle
 
 class MyProjectService(project: Project) {
 
